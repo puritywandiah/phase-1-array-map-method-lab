b517214805f0c5c99bbb52413191a0eb2f124535
+ <+>
2022-06-10 07:37:17 +0300

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,9 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(tutorial =>{
+    const dsi = tutorial.split(" ")
+    const diaper = dsi.map(word => word.charAt(0). toUpperCase() + word.slice(1))
+    return diaper.join ( " ");
+  });
 }
